60cf9c1da7f7350dbe3b05db4f921cd8480834cc
gitpanyu <1224721916@qq.com>
2019-08-12 11:05:00 +0800

    pan

---
+++ app/components/Main.js
@@ -0,0 +1,68 @@
+
+import React, { Component } from 'react'
+import RecentView from "./RecentView";
+import Chat from './Chat'
+import admin from './images/pan.jpg'
+import Home from './Home'
+import Counter from './Counter'
+
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+
+import Contact from "./Contact"
+
+import routes from "../constants/routes"
+document.body.className = "left";
+class Main extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected:'recent'
+    }
+    //this.selectNavigator = this.selectNavigator.bind(this)
+  }
+  selectNavigator (name) {
+    if (this.state.selected) {
+
+    }
+  }
+  render() {
+    return (
+      <div className="content">
+        <div className='main'>
+          <RecentView/>
+          <Chat/>
+        </div>
+        <div className='navigator'>
+          <div className="message_img">
+            <div className='message_L0'>
+            </div>
+            <img src={admin} id ='userimg'  className="message_L1"/>&nbsp;
+          </div>
+          <div className='message_body'>
+            <div className='message_body_L0'/>
+            <i id="recent" className="material-icons message_body_L1" onClick={(name)=> this.selectNavigator('recent')}>chat_bubble_outline</i>
+          </div>
+          <div style={{width:'50px'}}/>
+          <div className='message_body'>
+            <div className='message_body_L2'/>
+            <i id="recent" className="material-icons message_body_L1">toc</i>
+          </div>
+          <div style={{width:'50px'}}/>
+          <div className='message_body'>
+            <div className='message_body_L2'/>
+            <i id="recent" className="material-icons message_body_L1"onClick={(name)=> this.selectNavigator('mine')}>person_outline</i>
+          </div>
+          <div style={{flex:1}}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Main
